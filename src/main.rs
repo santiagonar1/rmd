aeb40b1bc21dcd23032e40698dd8f3615163e692
Santiago Narvaez <santiagonar1@gmail.com>
2021-04-27 13:22:28 +0200

    Add test store old forces grid

--- src/main.rs
+++ src/main.rs
@@ -283,4 +283,34 @@ mod tests {
         assert_eq!(grid.particles[0].velocity, particle1.velocity);
         assert_eq!(grid.particles[1].velocity, particle2.velocity);
     }
+
+    #[test]
+    fn can_store_old_forces() {
+        let force_first_particle = vec![4.0, 5.0, 6.0];
+        let force_second_particle = vec![7.0, 8.0, 9.0];
+
+        let particle1 = Particle {
+            mass: 20.0,
+            position: vec![0.0, 0.0, 0.0],
+            velocity: vec![1.0, 2.0, 3.0],
+            force: force_first_particle.clone(),
+            force_old: vec![0.0, 0.0, 0.0],
+        };
+
+        let particle2 = Particle {
+            mass: 40.0,
+            position: vec![1.0, 2.0, 3.0],
+            velocity: vec![1.0, 2.0, 3.0],
+            force: force_second_particle.clone(),
+            force_old: vec![0.0, 0.0, 0.0],
+        };
+
+        let mut grid = Grid {
+            particles: vec![particle1, particle2],
+        };
+
+        grid.store_old_forces();
+        assert_eq!(grid.particles[0].force_old, force_first_particle);
+        assert_eq!(grid.particles[1].force_old, force_second_particle);
+    }
 }
